626359b9a0315e5fb22c0006e923c08313cddb8a
unknown <ljrsqn@gmail.com>
2022-07-15 01:13:07 +0800

    layout update

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,33 +1,102 @@
-import React, {Component, Fragment} from 'react';
-import Navbar from '../../components/Navigation/Navbar/Navbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import React, { useState } from 'react';
+// import Navbar from '../../components/Navigation/Navbar/Navbar';
+// import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import {
+    Checkbox,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    Menu,
+    Segment,
+    Sidebar,
+} from 'semantic-ui-react'
 
-class Layout extends Component {
-    state = {
-        sideDrawer: false
-    }
-    drawerCloseHandler = () => {
-        this.setState({sideDrawer: false});
-    }
-    drawerHandler = () => {
-        this.setState((prevState) => {
-            return {sideDrawer: !prevState.sideDrawer};
-        });
-    }
-    render () {
-        return (
-            <Fragment>
-                <Navbar uid={this.props.uid} sid={this.props.sid} drawerToggle={this.drawerHandler} />
-                <SideDrawer 
-                    uid={this.props.uid} sid={this.props.sid}
-                    open={this.state.sideDrawer}
-                    close={this.drawerCloseHandler} />
-                <main>
-                    {this.props.children}
-                </main>
-            </Fragment>
-        )
-    }
+const Layout = (props) => {
+    const [visible, setVisible] = useState(false)
+    const [activeItem, setActiveItem] = useState()
+
+    // state = {
+    //     sideDrawer: false
+    // }
+    // drawerCloseHandler = () => {
+    //     this.setState({ sideDrawer: false });
+    // }
+    // drawerHandler = () => {
+    //     this.setState((prevState) => {
+    //         return { sideDrawer: !prevState.sideDrawer };
+    //     });
+    // }
+
+    return (
+        <>
+            {/* <Navbar uid={this.props.uid} sid={this.props.sid} drawerToggle={this.drawerHandler} /> */}
+            {/* <SideDrawer
+                uid={this.props.uid} sid={this.props.sid}
+                open={this.state.sideDrawer}
+                close={this.drawerCloseHandler} /> */}
+            <Menu size='massive' inverted>
+                <Menu.Item name='Populus'>
+                    <img alt="logo" src='https://react.semantic-ui.com/logo.png' />
+                </Menu.Item>
+                <Menu.Item>
+                    <code>Populus</code>
+                </Menu.Item>
+
+                <Menu.Menu position='right'>
+                    <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        onClick={() => setActiveItem('home')}
+                    />
+                    <Menu.Item
+
+                        name='profile'
+                        active={activeItem === 'profile'}
+                        onClick={() => setActiveItem('profile')}
+                    />
+                    <Menu.Item>
+                        {/* hide if in const laptop = ['md', 'lg', 'xl']; */}
+                        <Checkbox
+                            checked={visible}
+                            // label={{ children: <code>visible</code> }}
+                            onChange={(e, data) => setVisible(data.checked)}
+                        />
+                    </Menu.Item>
+                </Menu.Menu>
+            </Menu>
+            {/* hide if in const laptop = ['md', 'lg', 'xl']; */}
+            <Grid columns={1}>
+                <Grid.Column>
+                    <Sidebar.Pushable as={Segment}>
+                        <Sidebar
+                            as={Menu}
+                            animation='overlay'
+                            icon='labeled'
+                            inverted
+                            onHide={() => setVisible(false)}
+                            vertical
+                            visible={visible}
+                            width='thin'
+                        >
+                            <Menu.Item as='a'>
+                                <Icon name='home' />
+                                Home
+                            </Menu.Item>
+                            <Menu.Item as='a'>
+                                <Icon name='user circle' />
+                                Profile
+                            </Menu.Item>
+                        </Sidebar>
+
+                        <Sidebar.Pusher dimmed={visible}>
+                            {props.children}
+                        </Sidebar.Pusher>
+                    </Sidebar.Pushable>
+                </Grid.Column>
+            </Grid>
+        </>
+    )
 };
 
 export default Layout;
--- src/pages/people/people.view.js
+++ src/pages/people/people.view.js
@@ -3,10 +3,10 @@ import { useParams, Routes, Route } from "react-router-dom";
 import { PeopleProvider } from '../../contexts/people.context';
 import { PeopleService } from '../../services/people.service';
 import Error from '../../pages/error/error.view';
-// import Layout from '../../hoc/Layout/Layout';
-import { Placeholder, Container } from 'semantic-ui-react'
+import Layout from '../../hoc/Layout/Layout';
+import { Card, Image, Icon, Segment } from 'semantic-ui-react'
 import classes from './people.module.scss';
-import ProfileCard from '../../components/ProfileCard/ProfileCard';
+// import ProfileCard from '../../components/ProfileCard/ProfileCard';
 // import PersonalInfo from '../../components/Content/PersonalInfo/PersonalInfo';
 // import Home from '../../components/Content/Home/Home';
 // import Base from '../../components/Base/Base';
@@ -29,28 +29,34 @@ const People = () => {
         switch (people.status) {
             case 200:
                 return (
-                    // <Layout uid={uid}>
-                    <Container className={classes.Box}>
-                        {/* <ProfileCard uid={uid} />
+                    <Layout uid={uid}>
+                        <div className={classes.Box}>
+                            {/* <ProfileCard uid={uid} />
                             <Routes>
                                 <Route path="/profile/*" element={<PersonalInfo />} />
                                 <Route path="/home/*" element={<Home />} />
                             </Routes> */}
-                        <ProfileCard uid={uid} />
-                        <Placeholder>
-                            <Placeholder.Header image>
-                                <Placeholder.Line />
-                                <Placeholder.Line />
-                            </Placeholder.Header>
-                            <Placeholder.Paragraph>
-                                <Placeholder.Line />
-                                <Placeholder.Line />
-                                <Placeholder.Line />
-                                <Placeholder.Line />
-                            </Placeholder.Paragraph>
-                        </Placeholder>
-                    </Container>
-                    // </Layout>
+                            <Card>
+                                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+                                <Card.Content>
+                                    <Card.Header>Matthew</Card.Header>
+                                    <Card.Meta>
+                                        <span className='date'>Joined in 2015</span>
+                                    </Card.Meta>
+                                    <Card.Description>
+                                        Matthew is a musician living in Nashville.
+                                    </Card.Description>
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <a>
+                                        <Icon name='user' />
+                                        22 Friends
+                                    </a>
+                                </Card.Content>
+                            </Card>
+                            <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+                        </div>
+                    </Layout>
                 )
             case 503:
                 return <Error errorcode={"[503] Service Unavailable!"}
