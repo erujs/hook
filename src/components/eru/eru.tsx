58c4bc275a86b965179a9f89c0596da24eb21cbf
last-first <ljrsqn@gmail.com>
2022-10-13 23:06:26 +0800

    ui update

--- src/components/eru/eru.tsx
+++ src/components/eru/eru.tsx
@@ -1,15 +1,17 @@
 import styles from './eru.module.css';
 
 const Eru = () => {
+  const handleClick = () => {
+    window.open('https://github.com/Eru-js');
+  };
+
   return (
-    <div className={styles.div}
-      // onClick={handleClick}
-    >
+    <div className={styles.div} onClick={handleClick}>
       <code
       className={[styles.code, styles.dark].join(' ')}
       // className={[styles.code, color === 'dark' ? styles.light : styles.dark].join(' ')}
       >
-        dev with ♡ By Jrsqn
+        dev with ♡ By Eru
       </code>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,20 +22,11 @@ const Home: NextPage = () => {
               <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+            <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
               <ProfileCard />
             </main>
 
-            <footer className="flex h-24 w-full items-center justify-center border-t">
-              <a
-                className="flex items-center justify-center gap-2"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Powered by{' '}
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-              </a>
+            <footer className="flex h-24 w-full items-center justify-center">
               <Eru />
             </footer>
           </div>
