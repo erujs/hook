e975dfd059ee2a1faef2b909189faf0c14ce1fb6
erujs <a.m.jerus@gmail.com>
2024-03-14 07:35:28 +0800

    signature url update

--- src/components/eru/eru.tsx
+++ src/components/eru/eru.tsx
@@ -1,6 +1,6 @@
 export const Eru = () => {
   return (
-    <a href="https://github.com/Eru-js" target="_blank">
+    <a href="https://jerus.dev" target="_blank">
       <div className="flex justify-center cursor-pointer border border-solid border-1 border-black dark:border-white">
         <code className="z-1 p-2">
           dev with ♡ By Eru
